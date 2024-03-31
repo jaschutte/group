b55581d0758ee2ae6dd31bf4b05e4b695768b9a9
jaschutte <34577095+jaschutte@users.noreply.github.com>
2024-03-31 17:11:47 +0200

    Init

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use clap::Parser;
+use std::io::{Read, Write};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Operation {
+    #[arg(short, long, default_value = "\n\n")]
+    split: String,
+    #[arg(short, long, default_value = None)]
+    find: String,
+}
+
+fn main() {
+    // Get handles
+    let cli = Operation::parse();
+    let mut input = std::io::stdin();
+    let mut output = std::io::stdout();
+
+    // Retrieve STDIN
+    let mut data = String::with_capacity(2048);
+    input
+        .read_to_string(&mut data)
+        .expect("Unable to read stdin (data not UTF-8?)");
+
+    // Split the output
+    let grouped: String = data
+        .split(cli.split.as_str())
+        .filter(|group| group.contains(&cli.find))
+        .collect();
+
+    // Post the output back
+    output
+        .write(grouped.as_bytes())
+        .expect("Unable to write to stdout");
+    output.flush().expect("Unable to flush data to stdout");
+}
